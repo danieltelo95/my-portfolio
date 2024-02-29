4d1c1bd94568f9e3c3919de3f85677d6af292f7e
Daniel Tello <daniel.qui.tello@gmail.com>
2024-02-28 21:52:19 -0500

    estilos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import { IntlProvider } from 'react-intl';
 import Home from './pages/Home/Home';
 import messagesEn from './cv/cv_en.json';
 import messagesEs from './cv/cv_es.json';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import About from './pages/About/About';
+import Skills from './pages/Skills/Skills';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   const [userLanguage, setUserLanguage] = useState('es');
@@ -15,11 +20,15 @@ function App() {
   }
 
   return (
-    <IntlProvider locale={userLanguage}>
-      <div className="App">
-        <Home switchLanguage={switchLanguage} currentLanguage={userLanguage} />
-      </div>
-    </IntlProvider>
+    <Router>
+      <IntlProvider locale={userLanguage}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home switchLanguage={switchLanguage} currentLanguage={userLanguage} />} />
+          </Routes>
+        </div>
+      </IntlProvider>
+    </Router>
   );
 }
 
